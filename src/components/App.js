bdd2785880d3955505731a0d19b558785e199257
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-09-04 21:38:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,34 @@
-import React, { useState, useEffect, Fragment, useRef } from "react";
-import '../styles/App.css';
-
+import React, { useState, useRef } from "react";
 import List from "./List";
 
 const App = () => {
-
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
   const [list, setList] = useState([]);
-  const inputRef = useRef();
+  const inputRef = useRef(null);
 
   const onButtonClick = () => {
-    
-    setList([]);
-    for (let i = 1; i < Number(value) + 1; i++) {
-      setList((prev) => [...prev, i]);
+    const inputValue = inputRef.current.value;
+
+    if (inputValue === "") {
+      inputRef.current.focus();
+      return;
     }
-    setValue('')
+
+    setList(Array.from({ length: parseInt(inputValue) }, (_, i) => i + 1));
+    setValue("");
   };
-
-
- 
 
   return (
     <div id="main">
-
-      <input id="input" onChange={(e) => setValue(e.target.value)} value={value} ref={inputRef} />
-      <button id="button" onClick={onButtonClick}>Click</button>
+      <input
+        id="input"
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+        ref={inputRef}
+      />
+      <button id="button" onClick={onButtonClick}>
+        Click
+      </button>
       <ul id="list">
         <List listx={list} />
       </ul>
